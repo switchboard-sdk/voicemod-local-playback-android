2643c5b3d9a3a28b3fd6cbfd8755312f987598d4
Balazs Banto <balazsbanto@gmail.com>
2024-05-14 14:14:32 +0200

    renames

---
+++ app/src/main/java/audio/switchboard/voicemodlocalplayback/utils/SwitchboardConfig.kt
@@ -0,0 +1,7 @@
+package audio.switchboard.voicemodlocalplayback.utils
+
+// To initialize our SDK in your project, please get in touch to get your clientID and clientSecret values.
+// https://docs.switchboard.audio/downloads/
+const val switchboardClientID: String = "clientID"
+const val switchboardClientSecret: String = "clientSecret"
+const val voicemodLicenseKey: String = "QrTb5i2pCF"
